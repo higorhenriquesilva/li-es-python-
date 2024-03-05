ac455f2c93e592d4ab692e8ba67842ede54ba5b2
higor henrique da silva mastrangelo <126088507+higorhenriquesilva@users.noreply.github.com>
2024-03-04 21:54:13 -0300

    Add files via upload

---
+++ Alexandre-main/Alexandre-main/programa-o-alexandre-main/programa-o-alexandre-main/Ex1Trimeste/ex04.py
@@ -0,0 +1,9 @@
+print("preço da maça")
+quantidade = int(input("quantidade de maça"))
+maca = 2.40
+if(quantidade < 12):
+    maca = 2.80
+
+valor = quantidade * maca
+print("o valor é: {}" .format(valor))
+
---
+++ Alexandre-main/Alexandre-main/programa-o-alexandre-main/programa-o-alexandre-main/Ex1Trimeste/exercicio 3.py
@@ -0,0 +1,11 @@
+numero01 = int(input("digite o primeiro numero"))
+numero02 = int(input("digite o segundo numero"))
+
+if(numero01 > numero02):
+    print("O primeiro numero é maior que o segundo numero")
+elif(numero01 < numero02):
+    print("O segundo numero é maior que o primeiro numero")
+elif(numero01 == numero02):
+    print("os 2 numeros tem o mesmo valor")
+
+
